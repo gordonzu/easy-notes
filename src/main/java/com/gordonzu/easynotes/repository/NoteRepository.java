b8c0d4256141eada7e28e2d17790e63d49156d8b
gordonzu <gzulux@gmail.com>
2018-06-23 20:19:04 -0400

    initial_commit

---
+++ src/main/java/com/gordonzu/easynotes/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.gordonzu.easynotes.repository;
+
+import com.gordonzu.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
